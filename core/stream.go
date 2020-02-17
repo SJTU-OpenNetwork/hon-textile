667a839caca9ea9c47d9e595ef2f4b9fac3d03a9
Riften <943275366@qq.com>
2020-02-18 03:43:55 +0800

    Add Interface

---
+++ core/stream.go
@@ -0,0 +1,66 @@
+package core
+
+import (
+	stream "github.com/SJTU-OpenNetwork/go-stream"
+	path "github.com/SJTU-OpenNetwork/interface-go-ipfs-core/path"
+	"github.com/SJTU-OpenNetwork/hon-textile/pb"
+	"github.com/golang/protobuf/ptypes/any"
+	peer "github.com/libp2p/go-libp2p-core/peer"
+)
+
+func (t *Textile) StartStream(config stream.StreamConfig) error{
+	return nil
+}
+
+func (t *Textile) StreamAddFile(id uint64, path path.Path) error {
+	//solve path to ipld node
+
+	//call stream.Addfile
+	return nil
+}
+
+func (t* Textile) SubscribeStream(id uint64) error {
+	// call search stream
+
+	// swarm connect publisher
+
+	// call request stream
+
+	// call stream.StartWorker
+	return nil
+}
+
+func (t* Textile) UnsubscribeStream(id uint64) error{
+	return nil
+}
+
+// Handle request of streamid from peerid
+func (t *Textile) HandleRequestStream(streamid uint64, peerid peer.ID) error {
+
+	return nil
+}
+
+func (t *Textile) SearchStream(query *pb.StreamQuery, options *pb.QueryOptions) (<-chan *pb.QueryResult, <-chan error, *broadcast.Broadcaster, error) {
+	log.Debug("in SearchStream")
+	payload, err := proto.Marshal(query)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	options.Filter = pb.QueryOptions_HIDE_OLDER
+
+	resCh, errCh, cancel := t.search(&pb.Query{
+		Type:    pb.Query_STREAM,
+		Options: options,
+		Payload: &any.Any{
+			TypeUrl: "/StreamQuery",
+			Value:   payload,
+		},
+	})
+	return resCh, errCh, cancel, nil
+}
+
+//TODO:
+// - Add Stream into database
+// - Implement search for pb.Query_VIDEO in core/cafe_service.go/searchLocal.
+// - Add call back function to bitswap when initialize bitswap.
