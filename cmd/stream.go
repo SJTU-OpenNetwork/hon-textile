9758c67e0f4df953ac96804e9b0b9d5c0cfec2ef
Riften <943275366@qq.com>
2020-02-28 00:23:52 +0800

    update test command

--- cmd/stream.go
+++ cmd/stream.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 )
 
 func StreamCreate(streamId string, subNum int) error {
+	fmt.Printf("Call cmd/stream.go/StreamCreate")
 	cmdOpt := map[string]string{"streamId": streamId, "subNum": strconv.Itoa(subNum)}
 	res, err := executeStringCmd(http.MethodPost, "stream/create", params{opts: cmdOpt})
 	if err != nil {
