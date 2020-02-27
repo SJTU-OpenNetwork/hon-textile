1e9a9bef912df826572f4714ab8c166e3a533534
Riften <943275366@qq.com>
2020-02-28 00:29:40 +0800

    bug fix

--- cmd/stream.go
+++ cmd/stream.go
@@ -2,18 +2,18 @@ package cmd
 
 import (
 	"fmt"
-	"net/http"
-	"strconv"
+	//"net/http"
+	//"strconv"
 )
 
 func StreamCreate(streamId string, subNum int) error {
 	fmt.Printf("Call cmd/stream.go/StreamCreate")
-	cmdOpt := map[string]string{"streamId": streamId, "subNum": strconv.Itoa(subNum)}
-	res, err := executeStringCmd(http.MethodPost, "stream/create", params{opts: cmdOpt})
-	if err != nil {
-		return err
-	}
-	output(res)
+	//cmdOpt := map[string]string{"streamId": streamId, "subNum": strconv.Itoa(subNum)}
+	//res, err := executeStringCmd(http.MethodPost, "stream/create", params{opts: cmdOpt})
+	//if err != nil {
+	//	return err
+	//}
+	//output(res)
 
 	return nil
 }
