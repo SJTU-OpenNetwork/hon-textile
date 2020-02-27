eb9add3977e3334fa68b10f827a8fef1c84b90f4
Riften <943275366@qq.com>
2020-02-28 00:27:50 +0800

    update

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -951,12 +951,13 @@ The response contains a base58 encoded version of the random bytes token.`).Alia
 
 	// ================================
 	// For Stream
-	// TODO: Generage streamId instead of
+	// TODO: Generage streamId instead of set it.
 	streamCmd := appCmd.Command("stream", "File stream corresponding command.")
 	streamCreateCmd := streamCmd.Command("start", "Start a new file stream with specific streamid")
 	streamCreateId := streamCreateCmd.Arg("streamId", "Id of the creating stream.").Required().String()
 	streamCreateSubnum := streamCreateCmd.Flag("subNum", "Number of substreams.").Short('n').Default("1").Int()
 	cmds[streamCreateCmd.FullCommand()] = func() error {
+		fmt.Printf("Try to call StreamCreate")
 		return StreamCreate(*streamCreateId, *streamCreateSubnum)
 	}
 	//streamAddCmd := streamCmd.Command("add", "Add a file to stream")
