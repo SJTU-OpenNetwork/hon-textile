33c00f83d95c4973b9857c6e147d834d683ed850
Riften <943275366@qq.com>
2020-03-02 11:14:09 +0800

    remove notifee log

--- stream/StreamNotifiee.go
+++ stream/StreamNotifiee.go
@@ -1,8 +1,7 @@
 package stream
 
 import (
-	"fmt"
-	network "github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/network"
 	ma "github.com/multiformats/go-multiaddr"
 )
 
@@ -11,25 +10,25 @@ type StreamNotifee struct {
 }
 
 func (*StreamNotifee) Listen (net network.Network, addr ma.Multiaddr) {
-	fmt.Printf("Notifee: Listen address %s\n", addr.String())
+	//fmt.Printf("Notifee: Listen address %s\n", addr.String())
 }
 
 func (*StreamNotifee) ListenClose (net network.Network, addr ma.Multiaddr) {
-	fmt.Printf("Notifee: Close listen address %s\n", addr.String())
+	//fmt.Printf("Notifee: Close listen address %s\n", addr.String())
 }
 
 func (*StreamNotifee) Connected (net network.Network, conn network.Conn) {
-	fmt.Printf("Notifee: Connect %s\n", conn.RemotePeer().Pretty())
+	//fmt.Printf("Notifee: Connect %s\n", conn.RemotePeer().Pretty())
 }
 
 func (*StreamNotifee) Disconnected(net network.Network, conn network.Conn) {
-	fmt.Printf("Notifee: Disconnect %s\n", conn.RemotePeer().Pretty())
+	//fmt.Printf("Notifee: Disconnect %s\n", conn.RemotePeer().Pretty())
 }
 
 func (*StreamNotifee) OpenedStream(net network.Network, str network.Stream) {
-	fmt.Printf("Notifee: Open stream of protocol %s\n", str.Protocol())
+	//fmt.Printf("Notifee: Open stream of protocol %s\n", str.Protocol())
 }
 
 func (*StreamNotifee) ClosedStream(net network.Network, str network.Stream) {
-	fmt.Printf("Notifee: Close stream of protocal %s\n", str.Protocol())
+	//fmt.Printf("Notifee: Close stream of protocal %s\n", str.Protocol())
 }
