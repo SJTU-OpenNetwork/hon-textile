2d47b79db2d127abbad4fcb288ffeb1bd1bcf513
Riften <943275366@qq.com>
2020-05-25 21:22:22 +0800

    bug fix

--- cmd/simple_file.go
+++ cmd/simple_file.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func AddSimpleFile(path string, threadId string) error {
 	cmdOpt := map[string]string{"path": path, "threadId": threadId}
-	res, err := executeJsonPbCmd(http.MethodPost, "simpleFile/add", params{opts:cmdOpt}, nil)
+	res, err := executeJsonCmd(http.MethodPost, "simpleFile/add", params{opts:cmdOpt}, nil)
 	if err != nil {
 		return err
 	}
