fd6c1c0ecfbb9bfd533752a81ffe65968dabedbb
Riften <943275366@qq.com>
2020-06-21 20:20:21 +0800

    fix another stupid bug

--- recorder/record_service.go
+++ recorder/record_service.go
@@ -170,11 +170,11 @@ func (h *RecordService) handleRecordNotification(env *pb.Envelope, pid peer.ID) 
 			streamId, ok1 := block_map["ID"]
 			parentId, ok2 := block_map["Parent"]
 			if ok1 && ok2 {
-				err = h.trees.Add(streamId, parentId, h.peerId)
+				err = h.trees.Add(streamId, parentId, pid.Pretty())
 				if err != nil {
 					log.Error(err)
 				}
-				log.Debugf("Add tree node %s: %s -> %s", streamId, parentId, h.peerId)
+				log.Debugf("Add tree node %s: %s -> %s", streamId, parentId, pid.Pretty())
 			} else {
 				log.Errorf("Cannot parse tree info from %v", notification)
 			}
