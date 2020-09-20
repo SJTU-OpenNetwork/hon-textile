00e1d6262bc3144e0571e26b14ba78e4b86fbeea
GMXiao <a2471277747@qq.com>
2020-09-20 21:15:22 +0800

    debug

--- core/thread2_invite.go
+++ core/thread2_invite.go
@@ -76,20 +76,33 @@ func (t *Textile) handleInvite(env *pb.Envelope) error {
 		return err
 	}
 
+	threadId, err := thread2.Decode(inform.ThreadId)
+	if err != nil {
+		fmt.Println("error when decode string to threadid")
+		return err
+	}
+	err = t.NewMembersCollection(threadId)
+	if err != nil {
+		fmt.Println("error when new member collection")
+		return err
+	}
+	err = t.NewMessagesCollection(threadId)
+	if err != nil {
+		fmt.Println("error when new message collection")
+		return err
+	}
+
+
 	//add myself to member collection
 	//Start listening new created thread
 	t.ListenThread2s()
 	//add myself info to the thread collection of member
-	//threadId, err := thread2.Decode(inform.ThreadId)
-	//if err != nil {
-	//	fmt.Println("error when decode string to threadid")
-	//	return err
-	//}
-	//_, err = t.CreateMemInstance(threadId,  client.Instances{
-	//	ThreadMember{MemberId: t.Account().Address(), Name: t.Name(), Role: member}})
-	//if err != nil {
-	//	fmt.Println("Error when add myself info to the thread, ",err)
-	//	return err
-	//}
+
+	_, err = t.CreateMemInstance(threadId,  client.Instances{
+		ThreadMember{MemberId: t.Account().Address(), Name: t.Name(), Role: member}})
+	if err != nil {
+		fmt.Println("Error when add myself info to the thread, ",err)
+		return err
+	}
 	return nil
 }
