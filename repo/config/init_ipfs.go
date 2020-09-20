793a045dc76e63da373eadb4bf90333b65ab94d9
impleman <impleman@github.com>
2020-09-20 11:27:51 +0800

    p2p relay test

--- repo/config/init_ipfs.go
+++ repo/config/init_ipfs.go
@@ -76,6 +76,7 @@ var DefaultOpennetBootstrapAddresses = []string{
     "/ip4/119.3.24.157/tcp/4001/ipfs/QmYCYQMhyDJV4BU9fRr5xBzFDEccnukuViUT7GJLngP7fj",
     "/ip4/159.138.3.74/tcp/4001/ipfs/QmYovpcqB12c56AjGRaMUcwfoZg1DYinCFmEAzFHYvLb6R/",
 	"/ip4/159.138.130.129/tcp/4001/ipfs/QmZX8WVgJ3cQCW3bNcodXhmK34rmNkvqk8Zg9u7f3JEFgN",
+	"/ip4/106.12.102.87/tcp/3001/p2p/QmYSGmQwQo7PWZmGNv2DWEqLdP437Ks1f87xmpkqueucbU", // p2p relay
 }
 
 var OpennetCafeAddresses = []string{
