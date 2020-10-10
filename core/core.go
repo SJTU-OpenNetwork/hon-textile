c5129cdbb2ad5b47fa5ba7924805fb09a96146ff
GMXiao <a2471277747@qq.com>
2020-10-10 18:34:14 +0800

    debug

--- core/core.go
+++ core/core.go
@@ -601,7 +601,7 @@ func (t *Textile) CreateTCPPool() {
 func (t *Textile) watchMailBox() {
 	//fmt.Println("watch mail box")
 	for msg := range t.mail.Inbox {
-		//fmt.Println("New message arrive:", msg)
+		fmt.Println("New message arrive:", msg)
 		log.Infof("New message arrive:%s", msg)
 		// case thread invite:
 		//      t.handleInvite(msg)
