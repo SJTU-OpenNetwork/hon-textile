a272edd50c542f0beb1c476fccc8ad371178a4f3
Jerry-LX <chenlong.sjtu@gmail.com>
2020-10-22 14:07:28 +0800

    update

--- core/sync_files.go
+++ core/sync_files.go
@@ -1,68 +1,59 @@
 package core
 
-import (
-	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/ptypes/any"
-	"github.com/SJTU-OpenNetwork/hon-textile/broadcast"
-	"github.com/SJTU-OpenNetwork/hon-textile/pb"
-)
-
 /** Bug exists here
-  * If one device connects one cafe when adding but connects another 
-  * when removing, the removed file is still here.
-  */
+ * If one device connects one cafe when adding but connects another
+ * when removing, the removed file is still here.
+ */
 
+// func (t *Textile) SyncFile(file *pb.SyncFile) error {
+// 	err := t.datastore.SyncFiles().Add(file)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return t.PublishSyncFile(file)
+// }
 
-func (t *Textile) SyncFile(file *pb.SyncFile) error {
-    err := t.datastore.SyncFiles().Add(file)
-	if err != nil {
-		return err
-	}
-	return t.PublishSyncFile(file)
-}
+// func (t *Textile) AddSyncFile(file *pb.SyncFile) error {
+// 	return t.datastore.SyncFiles().Add(file)
+// }
 
-func (t *Textile) AddSyncFile(file *pb.SyncFile) error {
-    return t.datastore.SyncFiles().Add(file)
-}
+// func (t *Textile) PublishSyncFile(file *pb.SyncFile) error {
+// 	sessions := t.datastore.CafeSessions().List().Items
+// 	if len(sessions) == 0 {
+// 		return nil
+// 	}
+// 	for _, session := range sessions {
+// 		if err := t.cafe.PublishSyncFile(file, session.Id); err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
 
-func (t *Textile) PublishSyncFile(file *pb.SyncFile) error {
-	sessions := t.datastore.CafeSessions().List().Items
-	if len(sessions) == 0 {
-		return nil
-	}
-	for _, session := range sessions {
-		if err := t.cafe.PublishSyncFile(file, session.Id); err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// // SearchVideo searches the network for a video
+// func (t *Textile) SearchSyncFiles(query *pb.SyncFileQuery, options *pb.QueryOptions) (<-chan *pb.QueryResult, <-chan error, *broadcast.Broadcaster, error) {
+// 	payload, err := proto.Marshal(query)
+// 	if err != nil {
+// 		return nil, nil, nil, err
+// 	}
 
-// SearchVideo searches the network for a video
-func (t *Textile) SearchSyncFiles(query *pb.SyncFileQuery, options *pb.QueryOptions) (<-chan *pb.QueryResult, <-chan error, *broadcast.Broadcaster, error) {
-    payload, err := proto.Marshal(query)
-	if err != nil {
-		return nil, nil, nil, err
-	}
+// 	options.Filter = pb.QueryOptions_HIDE_OLDER
 
-	options.Filter = pb.QueryOptions_HIDE_OLDER
+// 	resCh, errCh, cancel := t.search(&pb.Query{
+// 		Type:    pb.Query_SYNC_FILE,
+// 		Options: options,
+// 		Payload: &any.Any{
+// 			TypeUrl: "/SyncFileQuery",
+// 			Value:   payload,
+// 		},
+// 	})
+// 	return resCh, errCh, cancel, nil
+// }
 
-	resCh, errCh, cancel := t.search(&pb.Query{
-		Type:    pb.Query_SYNC_FILE,
-		Options: options,
-		Payload: &any.Any{
-			TypeUrl: "/SyncFileQuery",
-			Value:   payload,
-		},
-	})
-	return resCh, errCh, cancel, nil
-}
-
-func (t *Textile) ListSyncFile(address string, fileType pb.SyncFile_Type) *pb.SyncFileList {
-	files := &pb.SyncFileList{Items: make([]*pb.SyncFile, 0)}
-	for _, c := range t.datastore.SyncFiles().ListByType(address, fileType) {
-		files.Items = append(files.Items, c)
-	}
-    return files
-}
-
+// func (t *Textile) ListSyncFile(address string, fileType pb.SyncFile_Type) *pb.SyncFileList {
+// 	files := &pb.SyncFileList{Items: make([]*pb.SyncFile, 0)}
+// 	for _, c := range t.datastore.SyncFiles().ListByType(address, fileType) {
+// 		files.Items = append(files.Items, c)
+// 	}
+// 	return files
+// }
--- mobile/sync_file.go
+++ mobile/sync_file.go
@@ -1,75 +1,68 @@
 package mobile
 
-import (
+// func (m *Mobile) SyncFile(file []byte) error {
+// 	if !m.node.Started() {
+// 		return core.ErrStopped
+// 	}
 
-	"github.com/golang/protobuf/proto"
-	"github.com/SJTU-OpenNetwork/hon-textile/core"
-	"github.com/SJTU-OpenNetwork/hon-textile/pb"
-)
+// 	model := new(pb.SyncFile)
+// 	if err := proto.Unmarshal(file, model); err != nil {
+// 		return err
+// 	}
+//     return m.node.SyncFile(model)
+// }
 
-func (m *Mobile) SyncFile(file []byte) error {
-	if !m.node.Started() {
-		return core.ErrStopped
-	}
-    
-	model := new(pb.SyncFile)
-	if err := proto.Unmarshal(file, model); err != nil {
-		return err
-	}
-    return m.node.SyncFile(model)
-}
+// func (m *Mobile) AddSyncFile(file []byte) error {
+// 	if !m.node.Started() {
+// 		return core.ErrStopped
+// 	}
 
-func (m *Mobile) AddSyncFile(file []byte) error {
-	if !m.node.Started() {
-		return core.ErrStopped
-	}
+// 	model := new(pb.SyncFile)
+// 	if err := proto.Unmarshal(file, model); err != nil {
+// 		return err
+// 	}
+//     return m.node.AddSyncFile(model)
+// }
 
-	model := new(pb.SyncFile)
-	if err := proto.Unmarshal(file, model); err != nil {
-		return err
-	}
-    return m.node.AddSyncFile(model)
-}
+// func (m *Mobile) PublishSyncFile(file []byte) error {
+// 	if !m.node.Started() {
+// 		return core.ErrStopped
+// 	}
 
-func (m *Mobile) PublishSyncFile(file []byte) error {
-	if !m.node.Started() {
-		return core.ErrStopped
-	}
+// 	model := new(pb.SyncFile)
+// 	if err := proto.Unmarshal(file, model); err != nil {
+// 		return err
+// 	}
+//     return m.node.PublishSyncFile(model)
+// }
 
-	model := new(pb.SyncFile)
-	if err := proto.Unmarshal(file, model); err != nil {
-		return err
-	}
-    return m.node.PublishSyncFile(model)
-}
+// func (m *Mobile) ListSyncFile(address string, fileType int32) ([]byte, error) {
+// 	if !m.node.Started() {
+// 		return nil, core.ErrStopped
+// 	}
 
-func (m *Mobile) ListSyncFile(address string, fileType int32) ([]byte, error) {
-	if !m.node.Started() {
-		return nil, core.ErrStopped
-	}
+// 	files := m.node.ListSyncFile(address, pb.SyncFile_Type(fileType))
+// 	return proto.Marshal(files)
+// }
 
-	files := m.node.ListSyncFile(address, pb.SyncFile_Type(fileType))
-	return proto.Marshal(files)
-}
+// func (m *Mobile) SearchSyncFiles(query []byte, options []byte) (*SearchHandle, error) {
+// 	if !m.node.Online() {
+// 		return nil, core.ErrOffline
+// 	}
 
-func (m *Mobile) SearchSyncFiles(query []byte, options []byte) (*SearchHandle, error) {
-	if !m.node.Online() {
-		return nil, core.ErrOffline
-	}
+// 	mquery := new(pb.SyncFileQuery)
+// 	if err := proto.Unmarshal(query, mquery); err != nil {
+// 		return nil, err
+// 	}
+// 	moptions := new(pb.QueryOptions)
+// 	if err := proto.Unmarshal(options, moptions); err != nil {
+// 		return nil, err
+// 	}
 
-	mquery := new(pb.SyncFileQuery)
-	if err := proto.Unmarshal(query, mquery); err != nil {
-		return nil, err
-	}
-	moptions := new(pb.QueryOptions)
-	if err := proto.Unmarshal(options, moptions); err != nil {
-		return nil, err
-	}
-
-	resCh, errCh, cancel, err := m.node.SearchSyncFiles(mquery, moptions)
-    if err != nil {
-        log.Warning(err)
-		return nil, err
-	}
-	return m.handleSearchStream(resCh, errCh, cancel)
-}
+// 	resCh, errCh, cancel, err := m.node.SearchSyncFiles(mquery, moptions)
+//     if err != nil {
+//         log.Warning(err)
+// 		return nil, err
+// 	}
+// 	return m.handleSearchStream(resCh, errCh, cancel)
+// }
