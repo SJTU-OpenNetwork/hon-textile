dee971e3e33c378a42bd315fb8b1a2fe9239d489
GMXiao <a2471277747@qq.com>
2020-11-24 11:40:48 +0800

    add directory

--- api/api_simple_file.go
+++ api/api_simple_file.go
@@ -34,3 +34,33 @@ func (a *Api) addSimpleFile(g *gin.Context) {
 	pbJSON(g, http.StatusOK, block)
 	return
 }
+
+func (a *Api) addSimpleDir(g *gin.Context) {
+	// Parse parameters
+	// params are defined in cmd/stream.go
+	opts, err := a.readOpts(g)
+	if err != nil {
+		a.abort500(g, err)
+		return
+	}
+	path, ok := opts["path"]
+	if !ok {
+		g.String(http.StatusBadRequest, "missing path")
+		return
+	}
+	threadId, ok := opts["threadId"]
+	if !ok {
+		g.String(http.StatusBadGateway, "missing threadId")
+		return
+	}
+
+	block, err := a.Node.AddSimpleDirectory(path, threadId)
+	if err != nil {
+		log.Error(err)
+		g.String(http.StatusBadRequest, "error occur")
+		return
+	}
+	log.Debugf("Api done add simple file\n%s", block.String())
+	pbJSON(g, http.StatusOK, block)
+	return
+}
